a52f20dd6c561987fbaec63f1cb1a95c8416b094
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-02 03:55:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,7 +5,6 @@ class BankAccount
   attr_reader :name
 
   def initialize(name, balance=(1000), status=("open"))
-    @bank_account = []
     @name = name
     @balance = balance
     @status = status
