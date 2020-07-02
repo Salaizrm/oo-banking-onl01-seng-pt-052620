e1dbc05304d8e53c4070cab1d3e6051dd9443421
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-02 04:21:18 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,34 +1,32 @@
-require "pry"
 class BankAccount
-
+  
   attr_accessor :balance, :status
   attr_reader :name
-
-  def initialize(name, balance=(1000), status=("open"))
+  
+  def initialize(name, balance=1000, status="open")
     @name = name
     @balance = balance
     @status = status
   end
-
-  def deposit(money)
-    @balance += money
+  
+  def deposit(amount)
+    self.balance += amount
   end
-
-  def withdrawal(money)
-    self.balance -= money
+  
+  def withdrawal(amount)
+    self.balance -= amount
   end
-
+  
   def display_balance
     "Your balance is $#{self.balance}."
   end
-
+  
   def valid?
-    balance > 0 && status == "open"
+    self.status == "open" && self.balance > 0 ? true : false
   end
-
+  
   def close_account
-    @status = "closed"
+    self.status = "closed"
   end
-
 
 end
