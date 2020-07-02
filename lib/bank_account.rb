357cb59ed3112a9c23dd5bddbea6fc84def703e1
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-02 03:43:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,9 +2,10 @@ class BankAccount
 
   attr_accessor :bank_account, :name, :status
 
-  def initialize(name, bank_account=(1000), status=("open"))
+  def initialize(name, balance=(1000), status=("open"))
     @bank_account = []
     @name = name
+    @balance = balance
     @status = status
   end
 
