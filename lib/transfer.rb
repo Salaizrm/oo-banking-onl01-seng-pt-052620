005d34fee40d701f59e64084d26b43a184a3b479
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-02 04:16:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,13 +21,20 @@ class Transfer
     if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
       @sender.deposit( @amount * -1 )
       @receiver.deposit( @amount )
       @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
     end
   end
 
