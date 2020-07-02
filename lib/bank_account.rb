b0cd6ec635b05e2c36e106f4f4d35cbe9b5b91be
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-02 03:56:08 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 require "pry"
 class BankAccount
 
-  attr_accessor :bank_account, :balance, :status
+  attr_accessor :balance, :status
   attr_reader :name
 
   def initialize(name, balance=(1000), status=("open"))
