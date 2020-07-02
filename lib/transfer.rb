98e9bd95adb4665dde0ec0a8dc53dece9ac514b6
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-02 04:17:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,24 +18,27 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 )
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
-  end
-
-  def reverse_transfer
-    if @status == "complete"
-      @sender.deposit( @amount )
-      @receiver.deposit( @amount * -1)
-      @status = "reversed"
-    end
-  end
+     if valid? && sender.balance > amount && self.status == "pending"
+       sender.withdrawal(self.amount)
+       receiver.deposit(self.amount)
+       self.status = "complete"
+     else
+       self.status = "rejected"
+       "Transaction rejected. Please check your account balance."
+     end
+   end
+   
+   def reverse_transfer
+     if valid? && receiver.balance > amount && self.status == "complete"
+       sender.deposit(self.amount)
+       receiver.withdrawal(self.amount)
+       self.status = "reversed"
+     else
+       self.status = "rejected"
+       "Transaction rejected. Please check your account balance."      
+     end
+   end
+   
+   
 
 end
